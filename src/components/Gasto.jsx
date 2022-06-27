93f69e15bcc596eacb8bbed7ed17c6e62728f8f1
Camilo Torres <racknarow1@gmail.com>
2022-06-27 14:37:23 -0500

    fORMATEAR fECUA

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
+import {formatearFecha} from '../helpers'
 
 const Gasto = ({gasto}) => {
 
-    const { categoria, nombre, cantidad, id } = gasto;
+    const { categoria, nombre, cantidad, id, fecha } = gasto;
 
   return (
     <div className="gasto sombra">
         <div className="contenido-gasto">
+            {/* IMAGEN */}
             <div className="descripcion-gasto">
                 <p className="categoria">{categoria}</p>
                 <p className="nombre-gasto">{nombre}</p>
-
+                <p className='fecha-gasto' >
+                    Agregado el: {''}
+                    <span className="">{formatearFecha(fecha)}</span>    
+                </p>
             </div>
+            
         </div>
+        <p className='cantidad-gasto'>${cantidad}</p>
     </div>
   )
 }
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -5,3 +5,15 @@ export const generarId = () => {
     const fecha = Date.now().toString(36);
     return random + fecha;
 }
+
+
+export const formatearFecha = (fecha) => {
+    const fechaNueva = new Date(fecha);
+    const opciones = {
+        year: 'numeric',
+        month: 'long',
+        day: '2-digit'
+    }
+
+    return fechaNueva.toLocaleDateString('es-ES', opciones);
+}
